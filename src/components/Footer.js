5c4d073be29e57a7a36fafdae875931966b03134
Shahzad <shahzad20022002@gmail.com>
2022-09-24 12:52:48 +0530

    commit

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='bg-white py-8 text-center text-black'>
+      <div className='container mx-auto'>
+        Copyright &copy; 2022. All rights reserved.
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
